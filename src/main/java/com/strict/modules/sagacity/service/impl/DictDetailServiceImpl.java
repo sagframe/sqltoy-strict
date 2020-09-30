16d737fe9859c7e5354a65722048cd8446587ae6
chenrenfei <102309713@qq.com>
2020-09-30 11:32:17 +0800

    更新

---
+++ src/main/java/com/strict/modules/sagacity/service/impl/DictDetailServiceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.strict.modules.sagacity.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.strict.modules.sagacity.service.DictDetailService;
+
+/**
+ * @project sqltoy-strict
+ * @description 请在此说明类的功能
+ * @author zhong
+ * @version v1.0, Date:2020-9-30
+ * @modify 2020-9-30,修改说明
+ */
+@Service("dictDetailService")
+public class DictDetailServiceImpl implements DictDetailService {
+
+}
---
+++ src/main/java/com/strict/modules/system/service/impl/OrganInfoServiceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.strict.modules.system.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.strict.modules.system.service.OrganInfoService;
+
+/**
+ * @project sqltoy-strict
+ * @description 请在此说明类的功能
+ * @author zhong
+ * @version v1.0, Date:2020-9-30
+ * @modify 2020-9-30,修改说明
+ */
+@Service("organInfoService")
+public class OrganInfoServiceImpl implements OrganInfoService {
+
+}
