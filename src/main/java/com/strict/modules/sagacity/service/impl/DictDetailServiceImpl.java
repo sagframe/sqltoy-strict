b8ed596454a36ab971b842bc956b69dec089dda1
chenrenfei <102309713@qq.com>
2020-10-14 09:44:56 +0800

    更新

--- src/main/java/com/strict/modules/sagacity/service/impl/DictDetailServiceImpl.java
+++ src/main/java/com/strict/modules/sagacity/service/impl/DictDetailServiceImpl.java
@@ -3,18 +3,30 @@
  */
 package com.strict.modules.sagacity.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.strict.modules.sagacity.dao.DictDetailDao;
 import com.strict.modules.sagacity.service.DictDetailService;
+import com.strict.modules.sagacity.vo.DictDetailVO;
 
 /**
  * @project sqltoy-strict
  * @description 请在此说明类的功能
- * @author zhong
+ * @author zhongxuchen
  * @version v1.0, Date:2020-9-30
  * @modify 2020-9-30,修改说明
  */
 @Service("dictDetailService")
 public class DictDetailServiceImpl implements DictDetailService {
+	@Autowired
+	private DictDetailDao dictDetailDao;
+
+	@Override
+	public List<DictDetailVO> getDictByType(String dictType) {
+		return dictDetailDao.getDictByType(dictType);
+	}
 
 }
